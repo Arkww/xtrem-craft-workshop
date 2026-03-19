f5a7c44e5d90d7158447b1eadb64ed48ed4dd6ae
B-a-r-r <clement.barriere@etu.u-bordeaux.fr>
2026-03-19 11:03:50 +0100

    Added exchange rate assertions

--- python/src/xterm_craft_workshop/bank.py
+++ python/src/xterm_craft_workshop/bank.py
@@ -6,20 +6,26 @@ from xterm_craft_workshop.money import Money
 
 
 class Bank:
-    def __init__(self, exchange_rate: Dict[str, float] = None):
-        self._exchange_rate = exchange_rate if exchange_rate is not None else {}
+    def __init__(self, pivot_currency: Currency, exchange_rate: Dict[str, float] = {}):
+        self._exchange_rate = exchange_rate
+        assert pivot_currency is not None, "Le pivot currency ne peut pas être None"
+        self._pivot_currency = pivot_currency
 
-    def add_exchange_rate(self, from_currency: Currency, to_currency: Currency, rate: float) -> None:
+    def add_exchange_rate(self, to_currency: Currency, rate: float) -> None:
         """
         Ajouter un taux d'échange (méthode métier)
         """
-        self._exchange_rate[f'{from_currency.value}->{to_currency.value}'] = rate
+        assert rate > 0, "Le taux d'échange doit être positif"
+        assert to_currency != self._pivot_currency, "Le from_currency doit être différent du pivot_currency"
+        exchange_rate_key = f'{self._pivot_currency.value}->{to_currency.value}'
+        assert exchange_rate_key not in self._exchange_rate, "Le taux d'échange existe déjà"
+        self._exchange_rate[exchange_rate_key] = rate
 
-    def remove_exchange_rate(self, from_currency: Currency, to_currency: Currency) -> None:
+    def remove_exchange_rate(self, to_currency: Currency) -> None:
         """
         Supprimer un taux d'échange (méthode métier)
         """
-        del self._exchange_rate[f'{from_currency.value}->{to_currency.value}']
+        del self._exchange_rate[f'{self._pivot_currency.value}->{to_currency.value}']
 
     def convert(self, money: Money, to_currency: Currency) -> Money:
         """
